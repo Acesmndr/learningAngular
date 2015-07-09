1f18f91f144107f213bc742c5c03fea6bcc0ad46
Aashish Manandhar <acesmndr@gmail.com>
2015-07-09 22:30:21 +0545

    Learning controller

---
+++ app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+(function(){
+
+var app=angular.module('myApp',[]);
+app.controller('StoreController',function(){
+	this.product=gem;
+});
+var gem={
+	name:'Aashish ',
+	price:2.95
+}
+})();
+
+
