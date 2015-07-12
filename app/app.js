fa7b5d24c091f6081eb801d68a2222e96444f4cf
Aashish Manandhar <acesmndr@gmail.com>
2015-07-12 22:22:48 +0545

    Directives with controllers and communication

--- app/app.js
+++ app/app.js
@@ -19,6 +19,50 @@ var gems=[{
 	price:2300,
 	canPurchase:true
 }];
+//simple directive
+app.directive("superman",function(){
+return {
+	restrict:'A',
+	template:"<div>Hello I'm superman</div>",
+	link:function(scope,element){
+		element.bind('mouseenter',function(){
+		console.log("I'm inside of you");
+	});
+}
+}
+});
+//directive with communication or inheritence
+app.directive("superhero",function(){
+return {
+	restrict:'E',
+	scope:{},
+	controller:function($scope){
+		$scope.abilities=[];
+		this.addStrength=function(){
+			$scope.abilities.push("strength");
+		};
+		this.addSpeed=function(){
+		};
+		this.addDeception=function(){
+			$scope.abilities.push("Deception");
+		};
+	},
+	link:function(scope,element){
+		element.bind("mouseenter",function(){
+			console.log(scope.abilities);
+	});
+	}
+}
+});
+app.directive("batman",function(){
+return{
+	restrict:'A',
+	require:'superhero',
+	link:function(scope,element,attrs,superheroCtrl){
+		superheroCtrl.addDeception();	
+	}
+	}
+});
 })();
 
 
